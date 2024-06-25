d4872b1e10333b2f6a81721915eecc3285010227
Brandon Arroyo <brandon.arroyo.2000@gmail.com>
2024-06-25 10:37:38 -0600

    Correccion makefile

--- tarea0_u2/setup.sh
+++ tarea0_u2/setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 apt-get update
-apt-get install -y make wget shellcheck
-wget https://github.com/gohugoio/hugo/releases/download/v0.127.0/hugo_0.127.0_linux-amd64.deb
-dpkg -i hugo_0.127.0_linux-amd64.deb
-rm hugo_0.127.0_linux-amd64.deb
+apt-get apt-get install -y hugo make shellcheck
